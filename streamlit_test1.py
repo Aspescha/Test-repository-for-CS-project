4b51f95618cf875c32341d7bacae9f55e9ed3052
Aspescha <kreiliger.julian@gmail.com>
2026-03-13 17:16:02 +0100

    Add files via upload

---
+++ streamlit_test1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("My First Streamlit App")
+st.write("Hello, World!")
+
+name = st.text_input("Enter your name:")
+if name:
+    st.write(f"Nice to meet you, {name}!")
+
